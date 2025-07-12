2d85c35c9402742fb3174df7c2f85674e2ff2bd7
Aaryan Verma <aryanverma.av2000@gmail.com>
2025-07-12 18:28:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="minimal-profile">
+      <img src="https://superbrynaaryan.s3.eu-north-1.amazonaws.com/26904.jpg" alt="Profile" className="profile-img" />
+      <h1 className="profile-name">Aaryan Verma</h1>
     </div>
   );
 }
